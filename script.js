b1c63b7784f32c63f55e6f758a9a44c815e8a003
Cajsa Chirstensen <cajsachirstensen@desktop-fp8j4h8.kea.local>
2019-06-12 13:30:34 +0200

    plus,minus

--- script.js
+++ script.js
@@ -17,3 +17,15 @@ function toggleMenu() {
         document.querySelector("#menuknap").textContent = "X";
     }
 }
+
+function sidenVises() {
+    console.log("siden vises");
+    document.querySelector("#mere").addEventListener("click", readMore);
+}
+
+function readMore() {
+    console.log('readMore:');
+    document.querySelector("#box").classList.toggle("stor");
+    document.querySelector("#box").classList.toggle("lille");
+
+}
